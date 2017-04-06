93e2b2cf80883b2c3e2d56d830c9634fe42bf6e4
holasola <yuhong54@163.com>
2017-04-06 16:57:40 +0800

    add login page

---
+++ src/components/pages/Login.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="login-wrap">
+        <div class="ms-title">后台管理系统</div>
+        <div class="ms-login">
+            <el-from :model="ruleForm" :rule="rules" ref="ruleForm" class="demo-ruleForm">
+                <el-form-item prop="username">
+                    <el-input v-model="ruleForm.username" placeholder="username"></el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input type="password" placeholder="password" v-model="ruleForm.password" @keyup.enter.native="submitForm('ruleForm')"></el-input>
+                </el-form-item>
+                <div class="login-btn">
+                    <el-button type="primary" @click="submitForm('ruleForm')">
+                        登录
+                    </el-button>
+                </div>
+                
+            </el-from>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                ruleForm: {
+                    username: '',
+                    password: ''
+                },
+                rules: {
+                    username: [{
+                        required: true,
+                        message: '请输入用户名',
+                        trigger: 'blur'
+                    }],
+                    password: [{
+                        required: true,
+                        message: '请输入密码',
+                        trigger: 'blur'
+                    }]
+                }
+            }
+        },
+        methods: {
+            submitForm(formName) {
+                const self = this;
+                self.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        alert('submit');
+                        localStorage.setItem('ms_username', self.ruleForm.username);
+                        self.$router.push('/readme');
+                    } else {
+                        console.log('error submit');
+                        return false
+                    }
+                })
+            },
+            resetForm(formName) {
+
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
