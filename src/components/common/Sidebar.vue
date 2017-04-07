838fb22c80222132a519df97527e7ae51b8d435b
holasola <yuhong54@163.com>
2017-04-07 14:56:24 +0800

    add sidebar

---
+++ src/components/common/Sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="sidebar">
+        <el-menu :default-active="onRoutes" class="el-menu-vertical-demo" theme="dark" unique-opended router>
+            <el-menu-item index="readme">
+                <i class="el-icon-setting"></i>自述
+            </el-menu-item>
+            <el-submenu index="2">
+                <template slot="title"><i class="el-icon-menu">表格</i></template>
+                <el-menu-item index="basetable">基础表格</el-menu-item>
+                <el-menu-item index="basetable">基础表格</el-menu-item>
+            </el-submenu>
+        </el-menu>
+    </div>
+</template>
+<script>
+</script>
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import header from '../components/common/header'
+import header from '../components/common/Header'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,7 @@ export default new Router({
         },
         {
             path: '/login',
+            // 路由懒加载
             component: resolve => require(['../components/pages/Login.vue'], resolve)
 
         }
